42c2931d45c75cb6ccbb35425a71995aa1fa227b
muirurii <pnkanuna@gmail.com>
2022-02-10 11:13:13 +0300

    removed unused modules

--- src/components/Pages/HomePage/Popular.jsx
+++ src/components/Pages/HomePage/Popular.jsx
@@ -1,4 +1,4 @@
-import { Navigation, Pagination, A11y, EffectFade, Autoplay } from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
@@ -6,17 +6,9 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import PopularCard from './PopularCard';
-import { useState,useEffect } from 'react';
-
 
 const Popular = ({places})=>{
-  const[slides,setSlides] = useState(1.5);
-
-  useEffect(()=>{
-    const width = window.innerWidth;
-      setSlides(width < 550 ? .8 : 1.5 );
-  },[]);
-
+ 
   return(
       <div id="popular-section">
         <h1>Popular Destinations</h1>
@@ -26,7 +18,6 @@ const Popular = ({places})=>{
             navigation
             autoplay={{delay:3000}}
             breakpoints = {
-              // when window width is >= 320px
               {
               // when window width is >= 480px
               480: {
@@ -35,17 +26,17 @@ const Popular = ({places})=>{
               },
               // when window width is >= 640px
               640: {
+                slidesPerView: 2,
+                spaceBetween: 30
+              },
+              980: {
                 slidesPerView: 3,
                 spaceBetween: 30
-              }}
+              }
+            }
             }
             onAutoplayResume = {true}
             pagination={{ clickable: true }}
-            effect= 'flip'
-            flipEffect = {{
-              slideShadows: false
-            }
-            }
             >
               {places.slice(0,5).map(place=>(
                <SwiperSlide className='popular' key={place.id} style={{"backgroundImage":`url(${place.background})`}}>
